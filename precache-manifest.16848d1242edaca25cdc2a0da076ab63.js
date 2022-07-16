38eed053bb67b971515fb1f6290950e0f6d88a3e
Patrick <patrick@Patricks-MacBook-Pro.local>
2022-07-16 11:56:23 +0200

    Updates

---
+++ precache-manifest.16848d1242edaca25cdc2a0da076ab63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2adb857d5fed72f62a39",
+    "url": "/PersonalityTest/static/js/main.2adb857d.chunk.js"
+  },
+  {
+    "revision": "cfc3b352882d5b711648",
+    "url": "/PersonalityTest/static/css/1.ee3a9dc6.chunk.css"
+  },
+  {
+    "revision": "cfc3b352882d5b711648",
+    "url": "/PersonalityTest/static/js/1.cfc3b352.chunk.js"
+  },
+  {
+    "revision": "750f16070f286deed25b",
+    "url": "/PersonalityTest/static/js/runtime~main.750f1607.js"
+  },
+  {
+    "revision": "cdc7f2a30ae67e825d2b72a043f7cc36",
+    "url": "/PersonalityTest/index.html"
+  }
+];
