e16233aa847bbaf4b890121eba948d6a49e299b4
Patrick <29860751+patrick-11@users.noreply.github.com>
2022-11-05 10:44:08 +0100

    Add lombok annotations

--- PersonalityTest-server/src/main/java/com/github/personalitytest/exception/ErrorResponse.java
+++ PersonalityTest-server/src/main/java/com/github/personalitytest/exception/ErrorResponse.java
@@ -1,31 +1,16 @@
 package com.github.personalitytest.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
-
+@AllArgsConstructor
+@Getter
+@Setter
 public class ErrorResponse {
 
   private String message;
   private List<String> details;
-
-  public ErrorResponse(String message, List<String> details) {
-    this.message = message;
-    this.details = details;
-  }
-
-  public String getMessage() {
-    return message;
-  }
-
-  public void setMessage(String message) {
-    this.message = message;
-  }
-
-  public List<String> getDetails() {
-    return details;
-  }
-
-  public void setDetails(List<String> details) {
-    this.details = details;
-  }
 }
