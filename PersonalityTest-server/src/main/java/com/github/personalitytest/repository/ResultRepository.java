058fe4d4e9bb5c35c7e9cf4c67e3e4e18b414c27
patrick-11 <29860751+patrick-11@users.noreply.github.com>
2022-11-27 18:40:23 +0100

    Add custom query to find result by user

--- PersonalityTest-server/src/main/java/com/github/personalitytest/repository/ResultRepository.java
+++ PersonalityTest-server/src/main/java/com/github/personalitytest/repository/ResultRepository.java
@@ -2,11 +2,16 @@ package com.github.personalitytest.repository;
 
 import com.github.personalitytest.model.Result;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 
 @Repository
 public interface ResultRepository extends JpaRepository<Result, UUID> {
+
+  @Query("SELECT r FROM results r WHERE r.user.id=?1")
+  List<Result> findByUser(UUID id);
 }
