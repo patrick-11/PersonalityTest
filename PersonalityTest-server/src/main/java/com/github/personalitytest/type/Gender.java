b35e09c5eab2dda371bb905f31dcf74a7e4b43f8
patrick-11 <29860751+patrick-11@users.noreply.github.com>
2022-11-27 18:45:19 +0100

    Add lombok getter

--- PersonalityTest-server/src/main/java/com/github/personalitytest/type/Gender.java
+++ PersonalityTest-server/src/main/java/com/github/personalitytest/type/Gender.java
@@ -1,6 +1,10 @@
 package com.github.personalitytest.type;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@AllArgsConstructor
+@Getter
 public enum Gender {
 
   MALE("Male"),
@@ -8,17 +12,9 @@ public enum Gender {
 
   private final String value;
 
-  Gender(String value) {
-    this.value = value;
-  }
-
-  public String getValue() {
-    return value;
-  }
-
   public static Gender fromName(String name) {
     if (name != null) {
-      for (Gender type : Gender.values()) {
+      for (var type : Gender.values()) {
         if (type.name().equalsIgnoreCase(name.trim())) {
           return type;
         }
