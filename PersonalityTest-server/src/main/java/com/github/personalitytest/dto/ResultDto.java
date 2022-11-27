903cc55f15059ac24eebd5512be7f0328996f99f
patrick-11 <29860751+patrick-11@users.noreply.github.com>
2022-11-27 18:33:53 +0100

    Add validation constraints to DTOs

--- PersonalityTest-server/src/main/java/com/github/personalitytest/dto/ResultDto.java
+++ PersonalityTest-server/src/main/java/com/github/personalitytest/dto/ResultDto.java
@@ -1,8 +1,14 @@
 package com.github.personalitytest.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.github.personalitytest.exception.ErrorResponse;
+import com.github.personalitytest.model.Result;
 import lombok.*;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.sql.Timestamp;
 import java.util.List;
 import java.util.UUID;
@@ -15,10 +21,15 @@ import java.util.UUID;
 public class ResultDto {
 
   private UUID id;
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
   private Timestamp completed;
   private UserDto user;
-  @NotNull(message = "Answers must have a value")
-  private List<Integer> answers;
+  @NotEmpty(message = ErrorResponse.RESULT_VALIDATION_ANSWER_EMPTY_ERROR)
+  @Size(min = Result.ANSWERS_SIZE, max = Result.ANSWERS_SIZE, message = ErrorResponse.RESULT_VALIDATION_ANSWER_SIZE_ERROR)
+  private List<
+      @DecimalMin(value = Result.ANSWERS_VALUE_MIN, message = ErrorResponse.RESULT_VALIDATION_ANSWER_VALUE_ERROR)
+      @DecimalMax(value = Result.ANSWERS_VALUE_MAX, message = ErrorResponse.RESULT_VALIDATION_ANSWER_VALUE_ERROR)
+          Integer> answers;
   private List<Double> results;
   private double avgScore;
 }
--- PersonalityTest-server/src/main/java/com/github/personalitytest/dto/UserDto.java
+++ PersonalityTest-server/src/main/java/com/github/personalitytest/dto/UserDto.java
@@ -1,9 +1,15 @@
 package com.github.personalitytest.dto;
 
+import com.github.personalitytest.exception.ErrorResponse;
+import com.github.personalitytest.model.User;
+import com.github.personalitytest.type.Gender;
+import com.github.personalitytest.validator.GenderValue;
 import lombok.*;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.UUID;
 
 @NoArgsConstructor
@@ -14,10 +20,13 @@ import java.util.UUID;
 public class UserDto {
 
   private UUID id;
-  @NotNull(message = "Name must have a value")
+  @NotEmpty(message = ErrorResponse.USER_VALIDATION_NAME_EMPTY_ERROR)
+  @Size(min = User.NAME_SIZE_MIN, max = User.NAME_SIZE_MAX, message = ErrorResponse.USER_VALIDATION_NAME_SIZE_ERROR)
   private String name;
-  @NotNull(message = "Gender must have a value")
+  @NotEmpty(message = ErrorResponse.USER_VALIDATION_GENDER_EMPTY_ERROR)
+  @GenderValue(clazz = Gender.class, message = ErrorResponse.USER_VALIDATION_GENDER_ERROR)
   private String gender;
-  @NotNull(message = "Age must have a value")
+  @DecimalMin(value = User.AGE_VALUE_MIN, message = ErrorResponse.USER_VALIDATION_AGE_SIZE_ERROR)
+  @DecimalMax(value = User.AGE_VALUE_MAX, message = ErrorResponse.USER_VALIDATION_AGE_SIZE_ERROR)
   private int age;
 }
