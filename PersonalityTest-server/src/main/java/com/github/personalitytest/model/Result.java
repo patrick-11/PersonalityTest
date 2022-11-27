11bb2ad316d083682184b338e8bfd76132e6253f
patrick-11 <29860751+patrick-11@users.noreply.github.com>
2022-11-27 18:35:36 +0100

    Add builder design pattern and constants

--- PersonalityTest-server/src/main/java/com/github/personalitytest/model/Result.java
+++ PersonalityTest-server/src/main/java/com/github/personalitytest/model/Result.java
@@ -12,35 +12,42 @@ import java.util.UUID;
 
 @Entity(name = "results")
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
 @Getter
 @Setter
 @ToString
 public class Result {
 
+  public static final int ANSWERS_SIZE = 10;
+  public static final String ANSWERS_VALUE_MIN = "1";
+  public static final String ANSWERS_VALUE_MAX = "7";
+
   @Id
   private UUID id;
   @CreationTimestamp
   private Timestamp completed;
-  @ManyToOne @JoinColumn
+  @ManyToOne
+  @JoinColumn
   private User user;
-  @ElementCollection(targetClass=Integer.class)
+  @ElementCollection(targetClass = Integer.class)
   private List<Integer> answers;
-  @ElementCollection(targetClass=Double.class)
+  @ElementCollection(targetClass = Double.class)
   private List<Double> results;
   private double avgScore;
 
   public void calculateResults() {
     List<Double> temp = new ArrayList<>();
-    temp.add((getAnswers().get(0) + reverse(getAnswers().get(5)))/2);
-    temp.add((getAnswers().get(6) + reverse(getAnswers().get(1)))/2);
-    temp.add((getAnswers().get(2) + reverse(getAnswers().get(7)))/2);
-    temp.add((getAnswers().get(8) + reverse(getAnswers().get(3)))/2);
-    temp.add((getAnswers().get(4) + reverse(getAnswers().get(9)))/2);
+    temp.add((getAnswers().get(0) + reverse(getAnswers().get(5))) / 2);
+    temp.add((getAnswers().get(6) + reverse(getAnswers().get(1))) / 2);
+    temp.add((getAnswers().get(2) + reverse(getAnswers().get(7))) / 2);
+    temp.add((getAnswers().get(8) + reverse(getAnswers().get(3))) / 2);
+    temp.add((getAnswers().get(4) + reverse(getAnswers().get(9))) / 2);
     setResults(temp);
   }
 
   public void calculateAvgScore() {
-    setAvgScore(getResults().stream().reduce(0.0, Double::sum)/getResults().size());
+    setAvgScore(getResults().stream().reduce(0.0, Double::sum) / getResults().size());
   }
 
   private double reverse(double value) {
--- PersonalityTest-server/src/main/java/com/github/personalitytest/model/User.java
+++ PersonalityTest-server/src/main/java/com/github/personalitytest/model/User.java
@@ -3,17 +3,27 @@ package com.github.personalitytest.model;
 import com.github.personalitytest.type.Gender;
 import lombok.*;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.util.Set;
 import java.util.UUID;
 
 
 @Entity(name = "users")
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
 @Getter
 @Setter
 @ToString
 public class User {
+
+  public static final int NAME_SIZE_MIN = 2;
+  public static final int NAME_SIZE_MAX = 10;
+  public static final String AGE_VALUE_MIN = "5";
+  public static final String AGE_VALUE_MAX = "120";
 
   @Id
   private UUID id;
