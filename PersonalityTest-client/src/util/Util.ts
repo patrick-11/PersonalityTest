e83c5cc5ee67d287fe2c59ca2bae51507644b5e4
patrick-11 <29860751+patrick-11@users.noreply.github.com>
2022-12-04 12:38:45 +0100

    Remove timestamp styler from util

--- PersonalityTest-client/src/util/Util.ts
+++ PersonalityTest-client/src/util/Util.ts
@@ -6,12 +6,3 @@ export const errorCheck = (error: any) => {
   }
   return "Server is not reachable at the moment!"
 }
-
-export const convertTimestamp = (timestamp: string) => {
-  const year = timestamp.substring(2, 4)
-  const month = timestamp.substring(5, 7)
-  const day = timestamp.substring(8, 10)
-  const hour = timestamp.substring(11, 13)
-  const minutes = timestamp.substring(14, 16)
-  return day + "." + month + "." + year + "-" + hour + ":" + minutes
-}
