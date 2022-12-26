94d58707f394f20e7504774aabdae0834ff4ace4
Patrick-11 <29860751+patrick-11@users.noreply.github.com>
2022-12-26 09:32:37 +0100

    [MapStruct] Add mapping and value mapping annotations for gender enum

--- PersonalityTest-server/src/main/java/com/github/personalitytest/mapper/ResultMapper.java
+++ PersonalityTest-server/src/main/java/com/github/personalitytest/mapper/ResultMapper.java
@@ -2,7 +2,10 @@ package com.github.personalitytest.mapper;
 
 import com.github.personalitytest.dto.ResultDto;
 import com.github.personalitytest.model.Result;
+import com.github.personalitytest.type.Gender;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ValueMapping;
 import org.mapstruct.factory.Mappers;
 
 
@@ -12,9 +15,13 @@ public interface ResultMapper extends MapperBasic<Result, ResultDto> {
   ResultMapper INSTANCE = Mappers.getMapper(ResultMapper.class);
 
   @Override
-    //@Mapping(source = "gender.value", target = "gender")
+  @Mapping(source = "user.gender.value", target = "user.gender")
   ResultDto toDto(Result entity);
 
   @Override
   Result toEntity(ResultDto dto);
+
+  @ValueMapping(source = "Male", target = "MALE")
+  @ValueMapping(source = "Female", target = "FEMALE")
+  Gender genderStringToGender(String genderString);
 }
