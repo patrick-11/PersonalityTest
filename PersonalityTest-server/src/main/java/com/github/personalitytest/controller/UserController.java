1f84c6c2012de4309c8e8b36d0646ec0de92a143
Patrick-11 <29860751+patrick-11@users.noreply.github.com>
2022-12-29 20:33:13 +0100

    [OpenAPIv3] Add operation and parameter description

--- PersonalityTest-server/src/main/java/com/github/personalitytest/controller/UserController.java
+++ PersonalityTest-server/src/main/java/com/github/personalitytest/controller/UserController.java
@@ -5,6 +5,8 @@ import com.github.personalitytest.dto.ResultDto;
 import com.github.personalitytest.dto.UserDto;
 import com.github.personalitytest.service.ResultService;
 import com.github.personalitytest.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,25 +26,33 @@ public class UserController implements ControllerBasic<UserDto> {
   private final UserService userService;
   private final ResultService resultService;
 
+  @Override
+  @Operation(summary = "Get all users")
   @GetMapping("/")
   public ResponseEntity<List<UserDto>> getAll() {
     return new ResponseEntity<>(userService.getAll(), HttpStatus.OK);
   }
 
   @Override
+  @Operation(summary = "Get an user by id")
   @GetMapping("/{id}")
-  public ResponseEntity<UserDto> get(@PathVariable("id") UUID id) {
+  public ResponseEntity<UserDto> get(@Parameter(description = "Id of user to be searched")
+                                     @PathVariable("id") UUID id) {
     return new ResponseEntity<>(userService.get(id), HttpStatus.OK);
   }
 
+  @Operation(summary = "Get all results of an user by id")
   @GetMapping("/{id}/results")
-  public ResponseEntity<List<ResultDto>> getResults(@PathVariable("id") UUID id) {
+  public ResponseEntity<List<ResultDto>> getResults(@Parameter(description = "Id of user to be searched")
+                                                    @PathVariable("id") UUID id) {
     return new ResponseEntity<>(resultService.getByUserId(id), HttpStatus.OK);
   }
 
   @Override
+  @Operation(summary = "Create an user")
   @PostMapping("/")
-  public ResponseEntity<UserDto> create(@Valid @RequestBody UserDto user) {
+  public ResponseEntity<UserDto> create(@Parameter(description = "User to be created")
+                                        @Valid @RequestBody UserDto user) {
     return new ResponseEntity<>(userService.create(user), HttpStatus.CREATED);
   }
 
@@ -51,25 +61,37 @@ public class UserController implements ControllerBasic<UserDto> {
     return null;
   }
 
+  @Operation(summary = "Create an user by id")
   @PostMapping("/{id}")
-  public ResponseEntity<UserDto> createById(@PathVariable("id") UUID id, @Valid @RequestBody UserDto user) {
+  public ResponseEntity<UserDto> createById(@Parameter(description = "Id of user to be created")
+                                            @PathVariable("id") UUID id,
+                                            @Parameter(description = "User to be created")
+                                            @Valid @RequestBody UserDto user) {
     return new ResponseEntity<>(userService.create(id, user), HttpStatus.CREATED);
   }
 
   @Override
+  @Operation(summary = "Update an user by id")
   @PutMapping("/{id}")
-  public ResponseEntity<UserDto> update(@PathVariable("id") UUID id, @Valid @RequestBody UserDto user) {
+  public ResponseEntity<UserDto> update(@Parameter(description = "Id of user to be updated")
+                                        @PathVariable("id") UUID id,
+                                        @Parameter(description = "User to be updated")
+                                        @Valid @RequestBody UserDto user) {
     return new ResponseEntity<>(userService.update(id, user), HttpStatus.OK);
   }
 
   @Override
+  @Operation(summary = "Delete an user by id")
   @DeleteMapping("/{id}")
-  public ResponseEntity<Boolean> delete(@PathVariable("id") UUID id) {
+  public ResponseEntity<Boolean> delete(@Parameter(description = "Id of user to be deleted")
+                                        @PathVariable("id") UUID id) {
     return new ResponseEntity<>(userService.delete(id), HttpStatus.OK);
   }
 
+  @Operation(summary = "Delete all results of an user by id")
   @DeleteMapping("/{id}/results")
-  public ResponseEntity<Boolean> deleteResults(@PathVariable("id") UUID id) {
+  public ResponseEntity<Boolean> deleteResults(@Parameter(description = "Id of user where results to be deleted")
+                                               @PathVariable("id") UUID id) {
     return new ResponseEntity<>(resultService.deleteByUserId(id), HttpStatus.OK);
   }
 }
