c08b1e9dca85b768385d3da27d6453541e8134aa
Patrick-11 <29860751+patrick-11@users.noreply.github.com>
2022-12-29 20:35:33 +0100

    [OpenAPIv3] Remove inheritance and add custom exception handler

--- PersonalityTest-server/src/main/java/com/github/personalitytest/exception/ExceptionController.java
+++ PersonalityTest-server/src/main/java/com/github/personalitytest/exception/ExceptionController.java
@@ -1,35 +1,45 @@
 package com.github.personalitytest.exception;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 
 
-@ControllerAdvice
-public class ExceptionController extends ResponseEntityExceptionHandler {
+@RestControllerAdvice
+public class ExceptionController {
 
   @ExceptionHandler(value = NotFoundException.class)
-  public ResponseEntity<Object> exception(NotFoundException exception) {
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  public ResponseEntity<ErrorResponse> handleNotFound(NotFoundException exception) {
     var details = new ArrayList<String>();
     details.add(exception.getLocalizedMessage());
-    return new ResponseEntity<>(new ErrorResponse(ErrorResponse.ENTRY_NOT_FOUND, details), HttpStatus.NOT_FOUND);
+    return generateErrorResponse(ErrorResponse.ENTRY_NOT_FOUND, details, HttpStatus.NOT_FOUND);
   }
 
-  @Override
-  protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception,
-                                                                HttpHeaders headers, HttpStatus status, WebRequest request) {
+  @ExceptionHandler(value = NotValidException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public ResponseEntity<ErrorResponse> handleNotValid(NotValidException exception) {
+    var details = new ArrayList<String>();
+    details.add(exception.getLocalizedMessage());
+    return generateErrorResponse(ErrorResponse.ENTRY_NOT_VALID, details, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(value = MethodArgumentNotValidException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(MethodArgumentNotValidException exception) {
     var details = new ArrayList<String>();
     exception.getBindingResult().getAllErrors().forEach(objectError -> details.add(objectError.getDefaultMessage()));
-    details.sort(Comparator.naturalOrder());
-    return new ResponseEntity<>(new ErrorResponse(ErrorResponse.ENTRY_NOT_VALID, details), HttpStatus.BAD_REQUEST);
+    return generateErrorResponse(ErrorResponse.ENTRY_NOT_VALID, details, HttpStatus.BAD_REQUEST);
   }
 
+  public ResponseEntity<ErrorResponse> generateErrorResponse(String message, ArrayList<String> details, HttpStatus httpStatus) {
+    details.sort(Comparator.naturalOrder());
+    return new ResponseEntity<>(new ErrorResponse(message, details), httpStatus);
+  }
 }
