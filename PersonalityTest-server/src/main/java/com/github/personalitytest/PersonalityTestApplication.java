30d394daae1cf92558d9ecfad11a304c905cdc6c
Patrick-11 <29860751+patrick-11@users.noreply.github.com>
2022-12-29 20:36:13 +0100

    [OpenAPIv3] Add SwaggerUI title

--- PersonalityTest-server/src/main/java/com/github/personalitytest/PersonalityTestApplication.java
+++ PersonalityTest-server/src/main/java/com/github/personalitytest/PersonalityTestApplication.java
@@ -1,13 +1,14 @@
 package com.github.personalitytest;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Personality Test"))
 public class PersonalityTestApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(PersonalityTestApplication.class, args);
   }
-
 }
